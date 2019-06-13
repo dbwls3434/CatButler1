0ef73ee9ef04a1a1fd682f29a04fb8eb8cc8cc7d
dbwls3434 <dbwls3434@gmail.com>
2019-06-13 21:15:42 +0900

    웹팩설정

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className='full-screen'>
+      <div>
+        <h1>SASS REACT</h1>
+        <br />
+        <a
+          className='button-line'
+          href='https://github.com/dbwls3434/CatButler1'
+          target='_blank'
+        >
+          Github 방문
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default App;
