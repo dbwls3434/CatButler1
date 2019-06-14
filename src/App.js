87571a06db26b1125b66eedb57fda627e672be6d
dbwls3434 <dbwls3434@gmail.com>
2019-06-14 20:24:20 +0900

    devConnector구조로 변경

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './scss/App.scss';
 
 const App = () => {
   return (
-    <div className='full-screen'>
-      <div>
-        <h1>SASS REACT</h1>
-        <br />
-        <a
-          className='button-line'
-          href='https://github.com/dbwls3434/CatButler1'
-          target='_blank'
-        >
-          Github 방문
-        </a>
-      </div>
-    </div>
+    <Fragment>
+      <nav class='navbar bg-dark'>
+        <h1>
+          <a href='dashboard.html'>
+            {' '}
+            <i class='fas fa-code' /> DevConnector{' '}
+          </a>
+        </h1>
+        <ul>
+          <li>
+            <a href='profiles.html'>Developers</a>
+          </li>
+          <li>
+            <a href='posts.html'>Posts</a>
+          </li>
+          <li>
+            <a href='dashboard.html'>
+              <i class='fas fa-user' />
+              <span class='hide-sm'> Dashboard</span>
+            </a>
+          </li>
+          <li>
+            <a href='login.html'>
+              <i class='fas fa-sign-out-alt' />
+              <span class='hide-sm'> Logout</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <section class='container'>
+        <h1 class='large text-primary'>HEADING</h1>
+        <p class='lead'>
+          <i class='fas fa-user' /> TEXT
+        </p>
+      </section>
+    </Fragment>
   );
 };
 
