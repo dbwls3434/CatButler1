f2e2742d59f7798e13ab878224f87fde334a2f53
dbwls3434 <dbwls3434@gmail.com>
2019-06-18 21:54:08 +0900

    menu change

--- src/App.js
+++ src/App.js
@@ -4,38 +4,44 @@ import './scss/App.scss';
 const App = () => {
   return (
     <Fragment>
-      <nav class='navbar bg-dark'>
+      <nav className='navbar bg-dark'>
         <h1>
-          <a href='dashboard.html'>
+          <a href='#!'>
             {' '}
-            <i class='fas fa-code' /> DevConnector{' '}
+            <i class="fas fa-cat"></i> CatButler{' '}
           </a>
         </h1>
-        <ul>
+        <ul className='nav-items'>
           <li>
-            <a href='profiles.html'>Developers</a>
+            <a href='#!'>Menu1</a>
           </li>
           <li>
-            <a href='posts.html'>Posts</a>
+            <a href='#!'>Menu2</a>
           </li>
           <li>
-            <a href='dashboard.html'>
-              <i class='fas fa-user' />
-              <span class='hide-sm'> Dashboard</span>
+            <a href='#!'>Menu3</a>
+          </li>
+          <li>
+            <a href='#!'>
+              <i className='fas fa-sign-in-alt' />
+              <span className='hide-sm'> Login</span>
             </a>
           </li>
           <li>
-            <a href='login.html'>
-              <i class='fas fa-sign-out-alt' />
-              <span class='hide-sm'> Logout</span>
+            <a href='#!'>
+              <i className='fas fa-sign-out-alt' />
+              <span className='hide-sm'> Logout</span>
             </a>
           </li>
         </ul>
+        <div className='nav-hamburger'>
+          <i className='fas fa-bars fa-2x'></i>
+        </div>
       </nav>
-      <section class='container'>
-        <h1 class='large text-primary'>HEADING</h1>
-        <p class='lead'>
-          <i class='fas fa-user' /> TEXT
+      <section className='container'>
+        <h1 className='large text-primary'>HEADING</h1>
+        <p className='lead'>
+          <i className='fas fa-user' /> TEXT
         </p>
       </section>
     </Fragment>
